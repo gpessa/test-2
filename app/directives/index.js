621603db92a993e7fa7e3975934cc449c5a09ca9
Gaetano Pessa <gaetano@backbase.com>
2016-04-08 10:05:26 +0200

    add some documentation

--- app/directives/index.js
+++ app/directives/index.js
@@ -1,9 +1,26 @@
 // Directive to create the product
-import productImage from './product-image';
+
+
+/*
+Directive to create a subscripton selector, it require a phone model id to work,
+it use all the others directive to display all the informations in a proper way
+and let the use chooce the appropriate subscription
+*/
 import subscriptionSelector from './subscription-selector';
+
+// Directive to create the product image and handle the change of view
+import productImage from './product-image';
+
+// Create a list of subscriptions
 import subscriptionsList from './subscriptions-list';
+
+// Create the button to subscribe
 import subscriptionsBuy from './subscription-buy';
+
+// Provide a recap of the costs
 import subscriptionsRecap from './subscription-recap';
+
+// Show a small element with the phone price
 import phonePrice from './phone-price';
 
 // Initialization of modules
--- app/index.js
+++ app/index.js
@@ -3,12 +3,16 @@ require('./css/theme.scss');
 import angular from 'angular';
 import ngResource from 'angular-resource';
 
+// Collecting directives
 import registerDirectives from './directives';
+//Collecting resources
 import registerResources from './resources';
+//Collecting filters
 import registerFilters from './filters';
 
 var ngModule = angular.module('app', ['ngResource']);
 
+// Registering resources into the module
 registerDirectives(ngModule);
 registerResources(ngModule);
 registerFilters(ngModule);
